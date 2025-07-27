520af603d8d0cfd61fb88505603f1d215b618545
Ishmael Mavundla <v7.vundla@gmail.com>
2025-07-27 18:21:46 +0200

    Refactor prop drilling to use Context API

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,20 +1,13 @@
 import React from 'react';
-import './App.css';
-import UserContext from './UserContext';          
-import ProfilePage from './ProfilePage';         
+import UserContext from './UserContext';
+import ProfilePage from './ProfilePage';
 
 function App() {
-  const user = {
-    name: 'Jane Doe',
-    email: 'jane.doe@example.com'
-  };
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
   return (
-    <UserContext.Provider value={user}>           
-      <div>
-        <h1>React useContext Example</h1>
-        <ProfilePage />                           
-      </div>
+    <UserContext.Provider value={userData}>
+      <ProfilePage />
     </UserContext.Provider>
   );
 }
