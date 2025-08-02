cc08e264c29b53182aaadf40c5b072296ec9ff8c
Ishmael Mavundla <v7.vundla@gmail.com>
2025-08-02 18:01:27 +0200

    Setup project structure

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div style={{ padding: "20px", maxWidth: "800px", margin: "auto" }}>
+      <h1>GitHub User Search</h1>
+      <p>Start building your search component here.</p>
+    </div>
+  );
+}
+
+export default App;
